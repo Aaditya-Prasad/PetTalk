74f2882242087c4311060819cb69072d3113214f
Aaditya-Prasad <50385321+Aaditya-Prasad@users.noreply.github.com>
2023-03-13 20:20:05 -0700

    whisper webhook actually works now

--- src/modal/stt.py
+++ src/modal/stt.py
@@ -1,19 +1,13 @@
 import modal
 from fastapi import FastAPI, File, UploadFile
+from pydantic import BaseModel
 
 stub = modal.Stub(image=modal.Image.debian_slim().pip_install("openai~=0.27.0"))
 
-app = FastAPI()
 
-@app.post("/stt")
-@stub.function(secret=modal.Secret.from_name("my-openai-secret"))
-async def stt(file: UploadFile = File(...)):
+@stub.webhook(method="POST", secret=modal.Secret.from_name("my-openai-secret") )
+def stt(file: UploadFile = File(...)):
     import openai
-    contents = await myfile.read()
     audio_file = open(file, "rb")
     response = openai.Audio.transcribe("whisper-1", audio_file)
-
-@stub.function(secret=modal.Secret.from_name("my-openai-secret"))
-#def ttt
-
-
+    return response
