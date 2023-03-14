1dbe5297819e1f439c3ef0cec43fafb5f95ccef5
Aaditya-Prasad <50385321+Aaditya-Prasad@users.noreply.github.com>
2023-03-14 03:13:19 -0700

    did a bunch of modal work

--- src/modal/od.py
+++ src/modal/od.py
@@ -2,13 +2,14 @@ import json
 import os
 
 import modal
+from fastapi import FastAPI, File, UploadFile
 
 stub = modal.Stub(image=modal.Image.debian_slim().pip_install("google-cloud-vision", "opencv-python-headless"))
 
 
-@stub.function(secret=modal.Secret.from_name("my-googlecloud-secret"),
+@stub.webhook(method="POST", secret=modal.Secret.from_name("my-googlecloud-secret"),
                 mounts=[modal.Mount.from_local_dir("pictures", remote_path="/root/pictures")])
-def query():
+def query(file: UploadFile = File(...)):
     from google.oauth2 import service_account
     from google.cloud import vision
     import numpy as np
@@ -18,8 +19,14 @@ def query():
     service_account_info = json.loads(os.environ["SERVICE_ACCOUNT_JSON"])
     credentials = service_account.Credentials.from_service_account_info(service_account_info)
 
-    # Run a query against a public dataset with name US first name statistics
-    def detect_labels(path):
+    def detect_labels(content):
+        """
+        Detects labels in the current file
+        This calls the google cloud vision api which is why we need credentials
+        Only the label descriptions are returned
+        Args: 
+        content: the image content.
+        """
         import io
         import os
         # Imports the Google Cloud client library
@@ -27,27 +34,28 @@ def query():
         # Instantiates a client
         client = vision.ImageAnnotatorClient(credentials = credentials)
         # The name of the image file to annotate
-        file_name = os.path.abspath(path)
-        # Loads the image into memory
-        with io.open(file_name, 'rb') as image_file:
-            content = image_file.read()
+        # file_name = os.path.abspath(path)
+        # # Loads the image into memory
+        # with io.open(file_name, 'rb') as image_file:
+        #     content = image_file.read()
         image = vision.Image(content=content)
         # Performs label detection on the image file
         response = client.label_detection(image=image)
         labels = response.label_annotations
-        return labels
+        return [t.description for t in labels]
     
-    def localize_objects(path):
-        """Localize objects in the local image.
+    def localize_objects(content):
+        """
+        Localize objects in the local image.
 
         Args:
-        path: The path to the local file.
+        content: the image content.
         """
         from google.cloud import vision
         client = vision.ImageAnnotatorClient(credentials = credentials)
 
-        with open(path, 'rb') as image_file:
-            content = image_file.read()
+        # with open(path, 'rb') as image_file:
+        #     content = image_file.read()
         image = vision.Image(content=content)
 
         objects = client.object_localization(
@@ -55,30 +63,32 @@ def query():
 
         return objects
     
-    def labels_to_array(labels):
-        return [t.description for t in labels]
-
-    def cutout(path):
-        img = cv.imread(path)
+    def cutout(content):
+        """
+        This takes the first detected object in the localized object
+        and cuts it out of the image so that we have a cropped picture 
+        of just the dog.
+        
+        Args:
+        content: the image content.
+        """
+        img = cv.imdecode(content)
         x, y, _ = img.shape
-        objs = localize_objects(path)
+        objs = localize_objects(content)
         ver = objs[0].bounding_poly.normalized_vertices[:]
         x_b = (int(ver[0].x * x), int(ver[1].x * x))
         y_b = (int(ver[1].y * y), int(ver[2].y * y))
-        return cutout, img[x_b[0]:x_b[1], y_b[0]:y_b[1]]
+        return (x_b, y_b), img[x_b[0]:x_b[1], y_b[0]:y_b[1]]
   
-    def compute(path):
-        labels = labels_to_array(detect_labels(path))
-        bbox, cut_img = cutout(path)
+    def compute(content):
+        #just calls everything
+        labels = detect_labels(content)
+        bbox, cut_img = cutout(content)
         return labels, bbox, cut_img
-    
-    a, b, c = compute('pictures/sammy.jpeg')
+
+    content = file.file.read()
+    a, b, c = compute(content)
     print("hello")
     print(a)
-    return a
+    return {"labels": a}
 
-
-if __name__ == "__main__":
-    with stub.run():
-        print("hi")
-        print(query.call())
--- src/modal/stt.py
+++ src/modal/stt.py
@@ -1,13 +1,35 @@
 import modal
+import os
 from fastapi import FastAPI, File, UploadFile
 from pydantic import BaseModel
+import tempfile
+import json
 
-stub = modal.Stub(image=modal.Image.debian_slim().pip_install("openai~=0.27.0"))
+stub = modal.Stub(image=modal.Image.debian_slim().pip_install("openai~=0.27.0", "python-multipart"))
 
 
 @stub.webhook(method="POST", secret=modal.Secret.from_name("my-openai-secret") )
 def stt(file: UploadFile = File(...)):
     import openai
-    audio_file = open(file, "rb")
+    # print(file.filename)
+    # print(file.content_type)
+    # print(file.file)
+    # print(file.file.name)
+    # print(os.getcwd())
+    # print(os.listdir())
+    with open("new_file.webm", "wb+") as new_file:
+        new_file.write(file.file.read())
+    audio_file= open("new_file.webm", "rb")
     response = openai.Audio.transcribe("whisper-1", audio_file)
-    return response
+    os.remove("new_file.webm")
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": "I am going to give you a transcript of an audio file and I want you to remove filler words and fix obvious mistakes in transcription. Return to me only your edited version of the transcript. Here is the transcript: " + response.text}
+        ]
+    )
+    response = response["choices"][0]["message"]["content"]
+    print(response)
+    return {"text": response}
+
