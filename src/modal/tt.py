9ff95163a33a528a4439d92a4e4a5f4c13087b96
Aaditya-Prasad <50385321+Aaditya-Prasad@users.noreply.github.com>
2023-03-15 00:41:06 -0700

    elevenlabs tt update

--- src/modal/tt.py
+++ src/modal/tt.py
@@ -3,8 +3,11 @@ import os
 from fastapi import FastAPI, UploadFile, File, HTTPException
 from pydantic import BaseModel
 import shelve
+import subprocess
+from starlette.responses import FileResponse
 
-stub = modal.Stub("tt", image=modal.Image.debian_slim().pip_install("google-cloud-vision", "opencv-python-headless", "python-multipart", "openai~=0.27.0"))
+
+stub = modal.Stub("tt", image=modal.Image.debian_slim().pip_install("google-cloud-vision", "opencv-python-headless", "python-multipart", "openai~=0.27.0", "elevenlabslib", "sounddevice", "pydub", "requires.io", "soundfile").apt_install("libportaudio2"))
 
 class Item(BaseModel):
     text: str
@@ -14,6 +17,9 @@ context = []
 @stub.webhook(method="POST", secret=modal.Secret.from_name("my-openai-secret") )
 def tt(item: Item):
     import openai
+    from elevenlabslib import ElevenLabsUser
+    from elevenlabslib.helpers import save_bytes_to_path
+    
     print(item.text) 
     context.append({"role": "user", "content": item.text})
     response = openai.ChatCompletion.create(
@@ -25,5 +31,9 @@ def tt(item: Item):
     response = response["choices"][0]["message"]["content"]
     print(response)
     context.append({"role": "assistant", "content": response})
-    return {"text": response}
+    user = ElevenLabsUser("2be25268d0c50becf9bf5f10645f50fb") #fill in your api key as a string
+    voice = user.get_voices_by_name("Rachel")[0]  #fill in the name of the voice you want to use. ex: "Rachel"
+    result = voice.generate_audio_bytes("hi") #fill in what you want the ai to say as a string
+    save_bytes_to_path("result.wav",result)
+    return FileResponse("result.wav", media_type='audio/wav', filename='result.wav')
 
